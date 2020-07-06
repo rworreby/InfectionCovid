cbc2ae5a064d7f37f4a4cbba10847d289ad5c374
ga48nuq <55765277+ga48nuq@users.noreply.github.com>
2020-07-06 14:28:57 +0200

    Add files via upload

---
+++ InfectionModel.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class InfectionModel(Model):
+    """A model for infection spread."""
+    
+    def __init__(self, N=10, width=10, height=10, ptrans=0.5, 
+                 progression_period=3, progression_sd=2, death_rate=0.0193, recovery_days=21,
+                 recovery_sd=7):
+        
+        self.num_agents = N
+        self.initial_outbreak_size = 1
+        self.recovery_days = recovery_days
+        self.recovery_sd = recovery_sd
+        self.ptrans = ptrans
+        self.death_rate = death_rate
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = RandomActivation(self)
+        self.running = True
+        self.dead_agents = []
+        # Create agents
+        for i in range(self.num_agents):
+            a = MyAgent(i, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+            #make some agents infected at start
+            infected = np.random.choice([0,1], p=[0.98,0.02])
+            if infected == 1:
+                a.state = State.INFECTED
+                a.recovery_time = self.get_recovery_time()
+            
+        self.datacollector = DataCollector(
+            #model_reporters={"Gini": compute_gini}, 
+            agent_reporters={"State": "state"})
+
+    def get_recovery_time(self):
+        return int(self.random.normalvariate(self.recovery_days,self.recovery_sd))
+    
+    def step(self):
+        self.datacollector.collect(self)
+        self.schedule.step()
+
+
+# In[ ]:
+
+
+def get_column_data(model):
+    #pivot the model dataframe to get states count at each step
+    agent_state = model.datacollector.get_agent_vars_dataframe()
+    X = pd.pivot_table(agent_state.reset_index(),index='Step',columns='State',aggfunc=np.size,fill_value=0)    
+    labels = ['Susceptible','Infected','Recovered']
+    X.columns = labels[:len(X.columns)]
+    return X
+    
+def plot_states(model,ax):    
+    steps = model.schedule.steps
+    X = get_column_data(model)
+    X.plot(ax=ax,lw=3,alpha=0.8)
+
+
+# In[2]:
+
+
+pop=300
+steps=100
+st=time.time()
+model = InfectionModel(pop, 20, 20, ptrans=0.5)
+for i in range(steps):
+    model.step()
+print (time.time()-st)
+agent_state = model.datacollector.get_agent_vars_dataframe()
+print (get_column_data(model))
+
+
+# In[ ]:
+
+
+f,ax=plt.subplots(1,1,figsize=(8,4))
+plot_states(model,ax)
+plt.savefig('SIR.png',dpi=150)
+
