2eb80485555e93eed3291c5ad4bac8e0a2079352
ga48nuq <55765277+ga48nuq@users.noreply.github.com>
2020-07-13 01:55:38 +0200

    update model

--- infection_spread/model.py
+++ infection_spread/model.py
@@ -15,7 +15,7 @@ from .agent import Human, Wall, Exit, Door
 class InfectionModel(Model):
     """A model for infection spread."""
 
-def __init__(self, N=10, width=10, height=10, ptrans=0.5, progression_period=3, progression_sd=2, death_rate=0.0193, recovery_days=21, recovery_sd=7, floor_plan_file, human_count, random_spawn, save_plots):
+def __init__(self, floor_plan_file, human_count, random_spawn, save_plots, N=10, width=10, height=10, ptrans=0.5, progression_period=3, progression_sd=2, death_rate=0.0193, recovery_days=21, recovery_sd=7):
         # Load floorplan
         # floorplan = np.genfromtxt(path.join("infection_spread/floorplans/", floor_plan_file))
         with open(os.path.join("infection_spread/floorplans/", floor_plan_file), "rt") as f:
@@ -28,6 +28,14 @@ def __init__(self, N=10, width=10, height=10, ptrans=0.5, progression_period=3, 
         width, height = np.shape(floorplan)
 
         # Init params
+        self.num_agents = N
+        self.initial_outbreak_size = 1
+        self.recovery_days = recovery_days
+        self.recovery_sd = recovery_sd
+        self.ptrans = ptrans
+        self.death_rate = death_rate
+        self.running = True
+        self.dead_agents = []
         self.width = width
         self.height = height
         self.human_count = human_count
@@ -95,7 +103,26 @@ def __init__(self, N=10, width=10, height=10, ptrans=0.5, progression_period=3, 
                 health = random.randint(self.MIN_HEALTH * 100, self.MAX_HEALTH * 100) / 100
                 speed = random.randint(self.MIN_SPEED, self.MAX_SPEED)
 
+         for i in range(self.num_agents):
+            a = Humant(i, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+            #make some agents infected at start
+            infected = np.random.choice([0,1], p=[0.98,0.02])
+            if infected == 1:
+                a.state = State.INFECTED
+                a.recovery_time = self.get_recovery_time()
+                    
 self.running = True
+self.datacollector = DataCollector(
+        #model_reporters={"Gini": compute_gini}, 
+        agent_reporters={"State": "state"})
+
+def get_recovery_time(self):
+    return int(self.random.normalvariate(self.recovery_days,self.recovery_sd))
 
 def step(self):
         """
@@ -108,3 +135,31 @@ def step(self):
             self.start_fire()
 
         self.datacollector.collect(self)
+
+def get_column_data(model):
+    #pivot the model dataframe to get states count at each step
+    agent_state = model.datacollector.get_agent_vars_dataframe()
+    X = pd.pivot_table(agent_state.reset_index(),index='Step',columns='State',aggfunc=np.size,fill_value=0)    
+    labels = ['Susceptible','Infected','Recovered']
+    X.columns = labels[:len(X.columns)]
+    return X
+    
+def plot_states(model,ax):    
+    steps = model.schedule.steps
+    X = get_column_data(model)
+    X.plot(ax=ax,lw=3,alpha=0.8)
+
+
+pop=300
+steps=100
+st=time.time()
+model = InfectionModel(pop, 20, 20, ptrans=0.5)
+for i in range(steps):
+    model.step()
+print (time.time()-st)
+agent_state = model.datacollector.get_agent_vars_dataframe()
+print (get_column_data(model))
+
+f,ax=plt.subplots(1,1,figsize=(8,4))
+plot_states(model,ax)
+plt.savefig('SIR.png',dpi=150)
