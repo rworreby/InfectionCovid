1007f87cf92d7b6b8a9b285adb8423195645ea45
worreby <robin@worreby.ch>
2020-08-03 00:04:46 +0200

    Formatting flake8

--- infection_spread/agent.py
+++ infection_spread/agent.py
@@ -1,16 +1,7 @@
-import time, enum
 import numpy as np
-import pandas as pd
-import pylab as plt
-import networkx as nx
-import sys
-import random
-from enum import Enum
+import enum
 
-from mesa import Agent, Model
-from mesa.time import RandomActivation
-from mesa.space import MultiGrid
-from mesa.datacollection import DataCollector
+from mesa import Agent
 
 
 class State(enum.IntEnum):
@@ -24,13 +15,13 @@ class Human(Agent):
     """ An agent in an epidemic model."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.age = self.random.normalvariate(20,40)        
-        self.state = State.SUSCEPTIBLE  
+        self.age = self.random.normalvariate(20, 40)
+        self.state = State.SUSCEPTIBLE
         self.infection_time = 0
         self.model = model
+
     def get_position(self):
         return self.pos
-
 
     def move(self):
         """Move the agent"""
@@ -44,40 +35,43 @@ class Human(Agent):
 
     def status(self):
         """Check infection status"""
-        
-        if self.state == State.INFECTED:     
+
+        if self.state == State.INFECTED:
             drate = self.model.death_rate
-            alive = np.random.choice([0,1], p=[drate,1-drate])
+            alive = np.random.choice([0, 1], p=[drate, 1-drate])
             if alive == 0:
-                self.model.schedule.remove(self)            
+                self.model.schedule.remove(self)
             t = self.model.schedule.time-self.infection_time
-            if t >= self.recovery_time:          
+            if t >= self.recovery_time:
                 self.state = State.RECOVERED
-            #print (self.model.schedule.time,self.recovery_time,t)
+            # print (self.model.schedule.time,self.recovery_time,t)
 
     def contact(self):
         """Find close contacts and infect"""
-        
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])       
+
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
-            #other = self.random.choice(cellmates)
+            # other = self.random.choice(cellmates)
             for other in cellmates:
-                #print (self.model.schedule.time,self.state,other.state)                
+                # print (self.model.schedule.time,self.state,other.state)
                 if self.random.random() > self.model.ptrans:
                     continue
-                if self.state is State.INFECTED and other.state is State.SUSCEPTIBLE:                    
+                elif self.state is State.INFECTED and \
+                        other.state is State.SUSCEPTIBLE:
                     other.state = State.INFECTED
                     other.infection_time = self.model.schedule.time
-                    other.recovery_time = model.get_recovery_time()
-                
+                    other.recovery_time = self.model.get_recovery_time()
+
     def step(self):
         self.status()
         self.move()
         self.contact()
 
+
 """
 ROOM SET UP
 """
+
 
 class FloorObject(Agent):
     def __init__(self, pos, traversable, model=None):
@@ -89,15 +83,18 @@ class FloorObject(Agent):
     def get_position(self):
         return self.pos
 
+
 class Door(FloorObject):
     def __init__(self, pos, model):
         super().__init__(pos, traversable=True, model=model)
         self.state = State.NA
 
+
 class Exit(FloorObject):
     def __init__(self, pos, model):
         super().__init__(pos, traversable=True, model=model)
         self.state = State.NA
+
 
 class Wall(FloorObject):
     def __init__(self, pos, model):
--- infection_spread/server.py
+++ infection_spread/server.py
@@ -3,7 +3,8 @@ from mesa.visualization.modules import CanvasGrid, ChartModule
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 from .model import InfectionModel
-from .agent import Exit, State, Wall, Human, Door
+from .agent import Exit, Wall, Human, Door
+
 
 # Creates a visual portrayal of the model in the browser interface
 def infection_spread_portrayal(agent):
@@ -37,6 +38,7 @@ def infection_spread_portrayal(agent):
 
     return portrayal
 
+
 # the grid size is fixed to 50x50
 canvas_element = CanvasGrid(infection_spread_portrayal, 50, 50, 800, 800)
 
@@ -46,16 +48,31 @@ status_chart = ChartModule([{"Label": "Susceptible", "Color": "blue"},
                             {"Label": "Recovered", "Color": "green"}])
 
 # Get list of available floorplans
-floor_plans = [f for f in listdir("infection_spread/floorplans") if path.isfile(path.join("infection_spread/floorplans", f))]
+floor_plans = [f for f in listdir("infection_spread/floorplans")
+               if path.isfile(path.join("infection_spread/floorplans", f))]
 
 # Specify the parameters changeable by the user, in the web interface
 model_params = {
-    "floor_plan_file": UserSettableParameter("choice", "Floorplan", value=floor_plans[0], choices=floor_plans),
-    "human_count": UserSettableParameter("number", "Number Of Human Agents", value=10),
-    "random_spawn": UserSettableParameter('checkbox', 'Spawn Agents at Random Locations', value=True),
-    "save_plots": UserSettableParameter('checkbox', 'Save plots to file', value=True)
+    "floor_plan_file": UserSettableParameter("choice", "Floorplan",
+                                             value=floor_plans[0],
+                                             choices=floor_plans
+                                             ),
+    "human_count": UserSettableParameter("number",
+                                         "Number Of Human Agents",
+                                         value=10
+                                         ),
+    "random_spawn": UserSettableParameter('checkbox',
+                                          'Spawn Agents at Random Locations',
+                                          value=True
+                                          ),
+    "save_plots": UserSettableParameter('checkbox',
+                                        'Save plots to file',
+                                        value=True)
 }
 
 # Start the visual server with the model
-server = ModularServer(InfectionModel, [canvas_element, status_chart], "Infection Model",
-                       model_params)
+server = ModularServer(InfectionModel,
+                       [canvas_element, status_chart],
+                       "Infection Model",
+                       model_params
+                       )
